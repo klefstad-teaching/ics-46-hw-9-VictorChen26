20cb575e400678e6a1e8db0afb170f8d68e2e84f
VictorChen26 <vchenjia@uci.edu>
2025-03-17 06:03:07 +0000

    check print word ladder

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -85,14 +85,14 @@ void load_words(set<string> & word_list, const string& file_name){
 
 void print_word_ladder(const vector<string>& ladder){
     if (ladder.empty()) {
-        cout << "No word ladder found." << endl;
+        cout << "No word ladder found.";
         return;
     }
-    cout << "Word ladder found: " << endl;
+    cout << "Word ladder found: ";
     for (size_t i = 0; i < ladder.size(); ++i) {
         cout << ladder[i];
         if (i < ladder.size() - 1)
-            cout << " -> ";
+            cout << " ";
     }
     cout << endl;
 }
